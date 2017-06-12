9ffab4a3653f6271b05597a62892d0ebe2806955
Victor de Chabaneix <vde-chab@student.42.fr>
2017-06-12 23:46:17 +0200

    Build eva.crx

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,7 +1,5 @@
 'use strict';
 
 document.body.querySelectorAll('*').forEach((e) => {
-    if(e.innerText && e.innerText.match(/Founded/)) {
-        e.innerText = e.innerText.replace(/Founded/, 'oh boy')
-    }
+  e.setAttribute('style', 'color: transparent!important')
 })
